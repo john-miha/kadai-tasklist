78efe9eaa81c8973d70ecd7f55f3e0e63c2e1fe6
john <sphyrna.job@gmail.com>
2022-03-17 05:58:18 +0000

    Add Login authentication function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :destroy, :show, :update]
+  
   def index
-      @tasks = Task.all
+    if logged_in?
+      @tasks = current_user.tasks.all
+    end
   end
 
   def show
@@ -13,7 +17,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    #old
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが正常に追加されました'
       redirect_to @task
@@ -51,6 +57,13 @@ private
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 end
