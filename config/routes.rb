bc36f7b003f05f4aebead230b17dd9d490b90a94
john <sphyrna.job@gmail.com>
2022-03-22 09:06:04 +0000

    Delete some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
-  resources :tasks
-  
+  resources :tasks, except: [:index]
   get 'login', to: 'sessions#new'
   post 'login', to:  'sessions#create'
   delete 'logout', to: 'sessions#destroy'
